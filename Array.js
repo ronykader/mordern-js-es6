b5c4a4fd1b6d92adaf71b9a9b86c71e1ec60ebe1
ronykader <rony.ksr06@gmail.com>
2021-07-24 16:30:29 +0600

    update multi array

--- Array.js
+++ Array.js
@@ -2,3 +2,18 @@
 
 let data = [1,2,3,4,5,6,7,8,9]
 console.log(data[1]);
+
+let multiArray = [
+  {
+    name: "Md.Tarikul Islam",
+    age: 22,
+    address: "House 7/2, Road No 06, Block A Dhaka 1216",
+  },
+  {
+    name: "Masud Parvej",
+    age: 20,
+    address: "Kazipara Dhaka 1216",
+  }
+];
+
+console.log(multiArray[0].name);
